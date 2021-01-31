dc0d1f0688d9da90958a184af7ed80ee3d3c98a9
Pablo Gómez <pablogomez@pablogomez.com>
2021-01-31 11:48:29 +0000

    lets drink beer

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 killall -q polybar
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 2; done
 if type "xrandr" > /dev/null; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar pbl -c ~/.config/polybar/config &
